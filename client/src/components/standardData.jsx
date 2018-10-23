ddf0a78f5bdc7c621f47ea70dbec748d1fe309a3
Adam <rainbowfloyd911@gmail.com>
2018-10-23 15:28:39 -0400

    Data rendering correctly, Basic add to cart button added

--- client/src/components/standardData.jsx
+++ client/src/components/standardData.jsx
@@ -11,21 +11,39 @@ const StandardData = (props) => {
         </tr>
         <tr>
           <th>NAME</th>
+            {props.products.map(function(product) {
+              return <td>{product.name}</td>
+            })}
         </tr>
         <tr>
           <th>ADD TO CART</th>
+            {props.products.map(function(product) {
+              return <td><button>ADD TO CART</button></td>
+            })}
         </tr>
         <tr>
           <th>RATING</th>
+            {props.products.map(function(product) {
+              return <td>{product.customer_rating}</td>
+            })}
         </tr>
         <tr>
           <th>PRICE</th>
+            {props.products.map(function(product) {
+              return <td>{product.price}</td>
+            })}
         </tr>
         <tr>
           <th>SHIPPING</th>
+            {props.products.map(function(product) {
+              return <td>{product.shipping_cost}</td>
+            })}
         </tr>
         <tr>
           <th>SOLD BY</th>
+            {props.products.map(function(product) {
+              return <td>{product.sold_by}</td>
+            })}
         </tr>
       </thead>
     </table>
