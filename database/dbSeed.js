be841bcb37b0b135cb29dac7b5a8da9c9198b436
RainbowFloyd <rainbowfloyd911@gmail.com>
2018-11-19 16:45:05 -0800

    Database seed now uses Promises

--- database/dbSeed.js
+++ database/dbSeed.js
@@ -2,23 +2,28 @@ const helpers = require('./databaseHelpers.js');
 
 const mainProducts = ["IPhone", "PreSonus AudioBox", "Smart Watch", "Jawbone Speaker", "Turtle Beach", "Wood Frame Sunglasses", "Phone Case", "3D Printer", "Phone Power Bank", "Drone", "Fitness Tracker", "Solar Panel", "Rechargeable Lighter", "E-Cig", "Rice Cooker", "Smart Water Detector", "Pressure Cooker", "Wireless Charging Pad", "Hair Dryer", "Robot Vacuum", "Knife Sharpener", "Shower Curtain Liner", "Stream Stink", "DNA Test", "Alexa"];
 
-const insertMainProducts = (products) => {
-  for (let i = 0; i < products.length; i++) {
-    let product = products[i];
-    helpers.insertMainProduct(product, function(result) {
-      console.log(result);
-    })
-  }
+
+//the helper functions are using callbacks while the top level function uses Promise
+const insertProduct = (product) => {
+  return new Promise((resolve, reject) => {
+    helpers.insertMainProduct(product, function(mainResult) {
+      console.log('main product inserted ', mainResult)
+      helpers.insertProductClone(product, function(cloneResult) {
+        console.log('product clones inserted ', cloneResult);
+        resolve();
+      });
+    });
+  });
 }
 
-const insertProductClone = (products) => {
-  for (let i = 0; i < products.length; i++) {
-    let product = products[i];
-    helpers.insertProductClone(product, function(result) {
-      console.log(result);
-    })
-  }
+
+let promises = [];
+
+
+// pushing a list of promises in an array to iterate over later
+for (let i = 0; i < mainProducts.length; i++) {
+  let product = mainProducts[i];
+  promises.push(insertProduct(product));
 }
 
-//insertMainProducts(mainProducts);
-insertProductClone(mainProducts);
+Promise.all(promises);
